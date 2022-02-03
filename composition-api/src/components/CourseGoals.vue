951e4d077ef136567f4f05f3bae5f354c97d0cd6
Jzone96 <jason.liberti@live.it>
2022-02-03 22:58:54 +0100

    aggiunta composition api

--- composition-api/src/components/CourseGoals.vue
+++ composition-api/src/components/CourseGoals.vue
@@ -8,32 +8,55 @@
 <script>
 import GoalsList from "./GoalsList.vue";
 import AddGoal from "./AddGoal.vue";
+import{ref,computed} from "vue"
 
 export default {
   components: {
     GoalsList,
     AddGoal,
   },
-  data() {
-    return {
-      goals: [],
-    };
-  },
-  computed: {
-    filteredGoals() {
-      return this.goals.filter(
+  setup (props) {
+    const goals = ref([]); //DATA 
+
+    const filteredGoals = computed (function(){//COMPUTED
+      return goals.value.filter(
         (goal) => !goal.text.includes("Angular") && !goal.text.includes("React")
       );
-    },
-  },
-  methods: {
-    addGoal(text) {
-      const newGoal = {
-        id: new Date().toISOString(),
-        text: text,
-      };
-      this.goals.push(newGoal);
-    },
-  },
+    });
+
+      function addGoal(text){//METHODS
+        const newGoal = {
+          id: new Date().toISOString(),
+          text: text,
+        };
+        goals.value.push(newGoal);
+      }
+
+    return{//ritorna tutto quello che deve essere disponibile nel template
+      filteredGoals,//infatti se cerchi filteredGoals, lo trovi nel template
+      addGoal,
+    }
+  }
+  // data() {
+  //   return {
+  //     goals: [],
+  //   };
+  // },
+  // computed: {
+  //   filteredGoals() {
+  //     return this.goals.filter(
+  //       (goal) => !goal.text.includes("Angular") && !goal.text.includes("React")
+  //     );
+  //   },
+  // },
+  // methods: {
+  //   addGoal(text) {
+  //     const newGoal = {
+  //       id: new Date().toISOString(),
+  //       text: text,
+  //     };
+  //     this.goals.push(newGoal);
+  //   },
+  // },
 };
 </script>
